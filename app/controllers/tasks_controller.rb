7374fd60f40619e860cf350f89fc9133fd1f2cc2
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-12 16:08:51 +0000

    created edit functionality and checkboxes showing if task is completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,41 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :destroy, :update]
   def index
-
-    @tasks = Task.all
-
+    @tasks = Task.all.order(created_at: :desc)
   end
 
   def create
+    Task.create(task_params)
+
+    redirect_to tasks_path
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def update
+    @task.update(task_params)
+    redirect_to task_path
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed, :permitted)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,18 @@ Rails.application.routes.draw do
 
   # # Read all
   # get 'tasks', to: 'tasks#index'
+
   # # Create
   # get 'tasks/new', to: 'tasks#new'
   # post 'tasks', to: 'tasks#create'
+
   # # Read one - Show route must be created *after* new route
   # get 'tasks/:id', to: 'tasks#show'
+
   # # Edit
   # get 'tasks/:id/edit', to: 'tasks#edit'
   # patch 'tasks/:id', to: 'tasks#update'
+
   # # Delete
   # delete 'tasks/:id', to: 'tasks#destroy'
 end
