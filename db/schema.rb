4e6feaa437769f679b6d55610435b775fad016f2
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-12 11:41:01 +0000

    updated tasks table columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_113303) do
+ActiveRecord::Schema.define(version: 2019_11_12_113847) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "string"
-    t.string "details"
-    t.string "text"
-    t.string "completed"
-    t.string "boolean", default: "f"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
