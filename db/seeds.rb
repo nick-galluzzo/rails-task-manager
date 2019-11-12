d3325285a5478e154a7a51076d00c60b02a77f1c
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-12 12:08:22 +0000

    seeding with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,6 +8,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Task.destroy_all
+Task.destroy_all
 
-# Task.create(title: , details: )
+10.times do
+  Task.create(title: Faker::Book.title, details: 'read this book')
+end
