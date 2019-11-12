690b201fa80c4d1e6b05683b7ee5aafed079b06e
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-12 12:02:56 +0000

    created routes and task#index method

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+
+
+  # # Read all
+  # get 'tasks', to: 'tasks#index'
+  # # Create
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # # Read one - Show route must be created *after* new route
+  # get 'tasks/:id', to: 'tasks#show'
+  # # Edit
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
